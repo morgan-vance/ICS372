75ce31e92d4964a7fc97586e9d1a69c098a80113
cj3108yq <46763867+cj3108yq@users.noreply.github.com>
2019-02-05 17:02:33 -0600

    Create Site.java

---
+++ Site.java
@@ -0,0 +1,41 @@
+
+public class Site {
+	private String locationID;
+	private String siteID;
+	private String rdgType;
+	private int rdgValue;
+	private String rdgDate;
+	
+	public String getLocationID() {
+		return locationID;
+	}
+	public void setLocationID(String locationID) {
+		this.locationID = locationID;
+	}
+	public String getSiteID() {
+		return siteID;
+	}
+	public void setSiteID(String siteID) {
+		this.siteID = siteID;
+	}
+	public String getRdgType() {
+		return rdgType;
+	}
+	public void setRdgType(String rdgType) {
+		this.rdgType = rdgType;
+	}
+	public int getRdgValue() {
+		return rdgValue;
+	}
+	public void setRdgValue(int rdgValue) {
+		this.rdgValue = rdgValue;
+	}
+	public String getRdgDate() {
+		return rdgDate;
+	}
+	public void setRdgDate(String rdgDate) {
+		this.rdgDate = rdgDate;
+	}
+	
+	
+}
